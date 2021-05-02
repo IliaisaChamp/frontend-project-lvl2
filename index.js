6cac2e5553c4b8daf6499fd0a82baa234bf69d6b
Ilia V <ilia.isachamp@gmail.com>
2021-05-02 18:25:51 +0300

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import { Command } from 'commander/esm.mjs';
 import makeCompareFiles from './src/main.js';
 
 const program = new Command();
-program
+export default program
   .description('Compares two configuration files and shows a difference.')
   .version('0.1.0')
   .option('-f, --format [type]', 'output format')
