5259241c17744d646d6e0b2cff6bffbac1b7e9e6
Ilia V <ilia.isachamp@gmail.com>
2021-05-02 19:21:37 +0300

    fix

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,4 +1,18 @@
 #!/usr/bin/env node
-import genDiff from '../index.js';
+import commander from 'commander';
+import makeCompareFiles from '../src/index.js';
 
-genDiff();
+const program = commander.createCommand();
+const genDiff = program
+  .description('Compares two configuration files and shows a difference.')
+  .version('0.1.0')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>>')
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    console.log(makeCompareFiles(filepath1, filepath2, program.format));
+  });
+
+program.parse(process.argv);
+
+export default genDiff;
