0973e19518fe01bf92f97bdfd3aff1eb5f6134c9
Ilia V <ilia.isachamp@gmail.com>
2021-05-03 14:02:13 +0300

    fix

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -6,7 +6,7 @@ import getDiff from '../index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', `/${filename}`);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', `${filename}`);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 const cases = [
