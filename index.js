8bb7f094b8b34afc18e5b2ade1c39245aae915fa
Ilia V <ilia.isachamp@gmail.com>
2021-05-03 15:55:39 +0300

    final fix

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ import format from './src/formatters/index.js';
 
 const makeDiff = (obj1, obj2) => {
   const keys = _.union(_.keys(obj1), _.keys(obj2));
-  const sortedKeys = [...keys].sort();
+  const sortedKeys = _.sortBy(keys);
 
   return sortedKeys.map((key) => {
     if (!_.has(obj1, key)) {
